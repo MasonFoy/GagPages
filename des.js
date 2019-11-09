7600339a675566f26793045f90e7f212df848974
Sam-Fong <57508144+Sam-Fong@users.noreply.github.com>
2019-11-08 23:10:56 -0700

    Update des.js

--- des.js
+++ des.js
@@ -1,84 +1,19 @@
 function testing(){
     document.getElementById("change").innerHTML = "New text!"
 }
-function testing01(){
-    document.getElementById("change").innerHTML = "It works";
-}
+
 function a(){
-    document.getElementById("change").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum diam lectus, convallis vestibulum tortor semper sit amet. Mauris eros elit, convallis eget mauris ac, consectetur facilisis massa. Donec id accumsan neque. Phasellus nec dolor sit amet nulla mollis scelerisque. Suspendisse finibus, nulla vel luctus condimentum, lectus leo semper neque, eget volutpat mauris dolor non dolor. Suspendisse mattis rhoncus sem, non sagittis eros faucibus in. Cras ac orci quis tortor aliquet ullamcorper mollis et leo. Morbi id risus id velit porttitor lacinia. In dignissim ullamcorper dui sed auctor.";
+    document.getElementById("change").innerHTML = "Jim Carrey's Bruce Almighty is a comedy about how a guy who comoplains about God often. He is then given the powers of God to teach him how hard it is to be the almighty one!";
 }
 function b(){
-    document.getElementById("change").innerHTML = "b";
+    document.getElementById("change").innerHTML = "Dumb and Dumber is about a dumb limo driver and his even dumber friend take a trip across the country to return a ladies briefcase that she left at an airport terminal";
 }
 function c(){
-    document.getElementById("change").innerHTML = "c";
+    document.getElementById("change").innerHTML = "A romantic comedy about Rachel Chu from New York flying to singapore to meet her boyfriends family( possibly one of the only good romantic comedies --- Colton)";
 }
 function d(){
-    document.getElementById("change").innerHTML = "d";
+    document.getElementById("change").innerHTML = "Follows the story of Truman who finds out that his life is actually a reality tv show";
 }
 function e(){
-    document.getElementById("change").innerHTML = "e";
-}
-function f(){
-    document.getElementById("change").innerHTML = "f";
-}
-function g(){
-    document.getElementById("change").innerHTML = "g";
-}
-function h(){
-    document.getElementById("change").innerHTML = "h";
-}
-function i(){
-    document.getElementById("change").innerHTML = "i";
-}
-function j(){
-    document.getElementById("change").innerHTML = "j";
-}
-function k(){
-    document.getElementById("change").innerHTML = "k";
-}
-function l(){
-    document.getElementById("change").innerHTML = "l";
-}
-function m(){
-    document.getElementById("change").innerHTML = "m";
-}
-function n(){
-    document.getElementById("change").innerHTML = "n";
-}
-function o(){
-    document.getElementById("change").innerHTML = "o";
-}
-function p(){
-    document.getElementById("change").innerHTML = "p";
-}
-function q(){
-    document.getElementById("change").innerHTML = "q";
-}
-function r(){
-    document.getElementById("change").innerHTML = "r";
-}
-function s(){
-    document.getElementById("change").innerHTML = "s";
-}
-function t(){
-    document.getElementById("change").innerHTML = "t";
-}
-function u(){
-    document.getElementById("change").innerHTML = "u";
-}
-function v(){
-    document.getElementById("change").innerHTML = "v";
-}
-function w(){
-    document.getElementById("change").innerHTML = "w";
-}
-function x(){
-    document.getElementById("change").innerHTML = "x";
-}
-function y(){
-    document.getElementById("change").innerHTML = "y";
-}
-function z(){
-    document.getElementById("change").innerHTML = "z";
+    document.getElementById("change").innerHTML = "a comedy about how a security guard has to contend with a gang of organized criminals who took over a shopping mall";
 }
