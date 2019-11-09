3f66d94d7d42b71fde2020a5869ab8176c89cea0
Sam-Fong <57508144+Sam-Fong@users.noreply.github.com>
2019-11-08 22:32:39 -0700

    Create des.js

---
+++ des.js
@@ -0,0 +1,84 @@
+function testing(){
+    document.getElementById("change").innerHTML = "New text!"
+}
+function testing01(){
+    document.getElementById("change").innerHTML = "It works";
+}
+function a(){
+    document.getElementById("change").innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum diam lectus, convallis vestibulum tortor semper sit amet. Mauris eros elit, convallis eget mauris ac, consectetur facilisis massa. Donec id accumsan neque. Phasellus nec dolor sit amet nulla mollis scelerisque. Suspendisse finibus, nulla vel luctus condimentum, lectus leo semper neque, eget volutpat mauris dolor non dolor. Suspendisse mattis rhoncus sem, non sagittis eros faucibus in. Cras ac orci quis tortor aliquet ullamcorper mollis et leo. Morbi id risus id velit porttitor lacinia. In dignissim ullamcorper dui sed auctor.";
+}
+function b(){
+    document.getElementById("change").innerHTML = "b";
+}
+function c(){
+    document.getElementById("change").innerHTML = "c";
+}
+function d(){
+    document.getElementById("change").innerHTML = "d";
+}
+function e(){
+    document.getElementById("change").innerHTML = "e";
+}
+function f(){
+    document.getElementById("change").innerHTML = "f";
+}
+function g(){
+    document.getElementById("change").innerHTML = "g";
+}
+function h(){
+    document.getElementById("change").innerHTML = "h";
+}
+function i(){
+    document.getElementById("change").innerHTML = "i";
+}
+function j(){
+    document.getElementById("change").innerHTML = "j";
+}
+function k(){
+    document.getElementById("change").innerHTML = "k";
+}
+function l(){
+    document.getElementById("change").innerHTML = "l";
+}
+function m(){
+    document.getElementById("change").innerHTML = "m";
+}
+function n(){
+    document.getElementById("change").innerHTML = "n";
+}
+function o(){
+    document.getElementById("change").innerHTML = "o";
+}
+function p(){
+    document.getElementById("change").innerHTML = "p";
+}
+function q(){
+    document.getElementById("change").innerHTML = "q";
+}
+function r(){
+    document.getElementById("change").innerHTML = "r";
+}
+function s(){
+    document.getElementById("change").innerHTML = "s";
+}
+function t(){
+    document.getElementById("change").innerHTML = "t";
+}
+function u(){
+    document.getElementById("change").innerHTML = "u";
+}
+function v(){
+    document.getElementById("change").innerHTML = "v";
+}
+function w(){
+    document.getElementById("change").innerHTML = "w";
+}
+function x(){
+    document.getElementById("change").innerHTML = "x";
+}
+function y(){
+    document.getElementById("change").innerHTML = "y";
+}
+function z(){
+    document.getElementById("change").innerHTML = "z";
+}
