ebd867222dae198c12cd57108af762dfeaf0a967
Bowdrie <57542295+Bowdrie@users.noreply.github.com>
2019-11-09 05:44:08 -0700

    Update des.js

--- des.js
+++ des.js
@@ -3,7 +3,7 @@ function testing(){
 }
 
 function a(){
-    document.getElementById("change").innerHTML = "Jim Carrey's Bruce Almighty is a comedy about how a guy who comoplains about God often. He is then given the powers of God to teach him how hard it is to be the almighty one!";
+    document.getElementById("change").innerHTML = "Jim Carrey's Bruce Almighty is a comedy about how a pretty selfish guy(if I do say so myself) who comoplains about God often(complaining to an omnipotent being... nice). He is then given the powers of God to teach him how hard it is to be the almighty one!";
 }
 function b(){
     document.getElementById("change").innerHTML = "Dumb and Dumber is about a dumb limo driver and his even dumber friend take a trip across the country to return a ladies briefcase that she left at an airport terminal";
@@ -12,8 +12,8 @@ function c(){
     document.getElementById("change").innerHTML = "A romantic comedy about Rachel Chu from New York flying to singapore to meet her boyfriends family( possibly one of the only good romantic comedies --- Colton)";
 }
 function d(){
-    document.getElementById("change").innerHTML = "Follows the story of Truman who finds out that his life is actually a reality tv show";
+    document.getElementById("change").innerHTML = "Follows the story of Truman who finds out that his life is actually a reality tv show(Which is actually kinda nice if ya think about it, I mean he probably didn't have to deal with much bullying at school or anything. He had an assured easy life and he screwed it up!).";
 }
 function e(){
-    document.getElementById("change").innerHTML = "a comedy about how a security guard has to contend with a gang of organized criminals who took over a shopping mall";
+    document.getElementById("change").innerHTML = "a comedy about how a slightly out of shape(who am I kidding, seriously out of shape)security guard has to contend with a gang of organized criminals who took over a shopping mall(I mean, they can take over a whole mall but a fat guy with non-lethals is just soooo far above their abilities?)";
 }
